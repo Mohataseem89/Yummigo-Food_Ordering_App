255f198310af5ebf6b88211b53faa843c875af50
Mohataseem Khan <mohataseem89@gmail.com>
2025-07-03 14:26:51 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import useActiveStatus from "../utils/useActiveStatus";
 import UserContext from "../utils/UserContext";
 import { useSelector } from "react-redux";
-import logo from '../assets/logo.png';
+
 
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");
@@ -29,8 +29,8 @@ const Header = () => {
         <Link to={"/"}>
           <img
             className="h-12 w-12 lg:h-16 lg:w-16 cursor-pointer rounded-full ring-2 ring-orange-400 hover:ring-orange-500 transition-all duration-300 hover:scale-105"
-{/*             src="/src/assets/logo.png" */}
-          src={logo} alt="Yummigo Logo" 
+            src="/src/assets/logo.png"
+          
             alt="Yummigo Logo"
           />
         </Link>
